d0416dea308fe7bdd77a0ef36389ffc8bd5c2481
AlinaBielska <alinkabelka5@gmail.com>
2023-06-04 00:06:53 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,28 +7,45 @@ import css from './App.module.css';
 
 export class App extends Component { 
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: ''
   };
   
   onInputChangeFilter = evt => {
     this.setState({ filter: evt.target.value });
   };
+
   onSubmitContact = contact => {
-    const { contacts } = this.state;
-    const newContact = { ...contact, id: nanoid() }
-    this.setState({
-      contacts: [...contacts, newContact]
-    });
+    // const { contacts } = this.state;
+    // const newContact = { ...contact, id: nanoid() }
+    // this.setState({
+    //   contacts: [...contacts, newContact]
+    // });
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { ...contact }],
+    }));
   };
+
   deleteContact = contactID => {
     this.setState({ contacts: this.state.contacts.filter(el => el.id !== contactID) });
   };
+
+  componentDidMount() {
+    const storedContacts = localStorage.getItem("contacts");
+    if (storedContacts === null) {
+      // localStorage.setItem("contacts", JSON.stringify([]));
+      console.log("hello");
+    } else {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem("contacts", JSON.stringify(contacts));
+    }
+  }
 
   render() {
     const { contacts, filter } = this.state;
